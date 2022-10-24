e91cdb564a8f8371d2853bf73c8c4470b1f618af
Dinos KONSTANTINOU <Dinos.KONSTANTINOU@sword-group.com>
2022-10-24 21:38:11 +0300

    Switched to UpperCase

--- gr-gettaxinfo/GetTaxInfo.cs
+++ gr-gettaxinfo/GetTaxInfo.cs
@@ -23,7 +23,7 @@ namespace gr.gsis.rgwspublic
     {
 
 
-        [FunctionName("getTaxInfo")]
+        [FunctionName("GetTaxInfo")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log)
